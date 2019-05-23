4a02822d189e78a1167a3f7959f57480c1ab9492
suyangsong <suyangsong@gmail.com>
2019-05-23 12:24:42 -0400

    added server scaffolding

--- client/src/components/Inventory/Inventory.js
+++ client/src/components/Inventory/Inventory.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class Inventory extends Component{
-
-    render(){
-        //testing routes
-        return(
-            <>
-                <h1>Hello from Inventory</h1>
-                <h2>Test ID : {this.props.match.params.inventoryId}</h2>
-            </>
-        )
-
-    }
+class Inventory extends Component {
+  render() {
+    //testing routes
+    return (
+      <>
+        <h1>Hello from Inventory</h1>
+        <h2>Test ID : {this.props.match.params.inventoryId}</h2>
+      </>
+    );
+  }
 }
 
 export default Inventory;
