a2b9bd12b97010cd5c60b3a7ebb2c7aff5fb7ddf
Andre Lowy <andre.lowy@gmail.com>
2019-05-23 20:35:21 -0400

    InventoryItem maps properly. Inventory is responsive at all sizes.

--- client/src/components/Inventory/Inventory.js
+++ client/src/components/Inventory/Inventory.js
@@ -2,27 +2,49 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
 import "./Inventory.scss";
-// import InventoryItem from "../InventoryItem/InventoryItem";
+import plusImg from "../../assets/Icons/SVG/Icon-add.svg";
+// import kebabMenu from "../../assets/Icons/SVG/Icon-kebab-default.svg";
+import InventoryItem from "../InventoryItem/InventoryItem";
 
 class Inventory extends Component {
   state = {
     inventory: [{}]
   };
 
-  getWarehouseData() {
-    Axios.get("http://localhost:8080/inventory")
+  getInventoryData(warehouseId) {
+    let id = warehouseId ? warehouseId : "";
+    Axios.get(`http://localhost:8080/inventory/${id}`)
       .then(res => {
-        this.setState({
-          inventory: this.res.data
-        });
+        if (id) {
+          this.setState({
+            inventory: [this.res.data]
+          });
+        } else {
+          this.setState({
+            inventory: this.res.data
+          });
+        }
       })
       .catch(err => {
         console.log(err);
+        // this.props.history.push('/errors')
       });
   }
 
   componentDidMount() {
-    this.getWarehouseData();
+    if (this.props.match.params.warehouseId) {
+      this.getInventoryData(this.props.match.params.warehouseId);
+    } else {
+      this.getInventoryData();
+    }
+  }
+
+  showRemoveBtn = e => {
+    console.log(e.target);
+  };
+
+  addInventoryItem = (e) => {
+
   }
 
   render() {
@@ -34,10 +56,12 @@ class Inventory extends Component {
           <input
             type="text"
             className="inventory__header-search"
-            placeholder="search"
+            placeholder="Search.."
           />
           <ul className="inventory__header-list">
-            <li className="inventory__header-list-heading">item</li>
+            <li className="inventory__header-list-heading inventory__header-list-heading--wide">
+              item
+            </li>
             <li className="inventory__header-list-heading">last ordered</li>
             <li className="inventory__header-list-heading">location</li>
             <li className="inventory__header-list-heading">quantity</li>
@@ -46,10 +70,17 @@ class Inventory extends Component {
         </div>
 
         <div className="inventory__list-container">
-          {/* {this.state.inventory.map( (item, i) => <InventoryItem key={i} item={item} />)} */}
-          <Link className="inventory__list-link" to="id">
+          {this.state.inventory.map( (item, i) => <InventoryItem key={i} item={item} showRemoveBtn={this.showRemoveBtn} />)}
+          {/* <Link className="inventory__list-link" to="id">
             <ul className="inventory__list-entry">
-              <li className="inventory__list-entry-heading">item</li>
+              <li className="inventory__list-entry-heading inventory__list-entry-heading--with-kebab">
+                item
+                <img
+                  src={kebabMenu}
+                  className="inventory__list-entry-heading-kebab"
+                  alt="remove item option"
+                />
+              </li>
               <li className="inventory__list-entry-item-product">
                 Product Name here
               </li>
@@ -64,8 +95,22 @@ class Inventory extends Component {
               <li className="inventory__list-entry-item">12,000</li>
               <li className="inventory__list-entry-heading">status</li>
               <li className="inventory__list-entry-item">In Stock</li>
+              <li className="inventory__list-entry-item-kebab">
+                <img
+                  src={kebabMenu}
+                  className="inventory__list-entry-item-kebab-img"
+                  alt="remove item option"
+                />
+              </li>
             </ul>
-          </Link>
+          </Link> */}
+          <div className="inventory__add-item">
+            <img
+              src={plusImg}
+              alt="add item plus"
+              className="inventory__add-item-img"
+            />
+          </div>
         </div>
       </section>
     );
--- client/src/components/InventoryItem/InventoryItem.js
+++ client/src/components/InventoryItem/InventoryItem.js
@@ -1,29 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import kebabMenu from "../../assets/Icons/SVG/Icon-kebab-default.svg";
 
-// const InventoryItem = ({ id, name, description, quantity, lastOrdered, location, isInStock }) => {
-// function InventoryItem (props) {
-class InventoryItem extends Component {
-
-    render() {
-        return(
-            <Link className="inventory__list-link" to='id'>
-                <ul className="inventory__list-entry">
-                    <li className="inventory__list-entry-heading">item</li>
-                    <li className="inventory__list-entry-item-product">{this.props.name}</li>
-                    <li className="inventory__list-entry-item-description">{this.props.description}</li>
-                    <li className="inventory__list-entry-heading">last ordered</li>
-                    <li className="inventory__list-entry-item">{this.props.lastOrdered}</li>
-                    <li className="inventory__list-entry-heading">location</li>
-                    <li className="inventory__list-entry-item">{this.props.location}</li>
-                    <li className="inventory__list-entry-heading">quantity</li>
-                    <li className="inventory__list-entry-item">{this.props.quantity}</li>
-                    <li className="inventory__list-entry-heading">status</li>
-                    <li className="inventory__list-entry-item">{this.props.isInStock ? "In Stock" : "Out of Stock"}</li>
-                </ul>
-            </Link>
-        )
-    }
-}
+const InventoryItem = ({
+  item: { id, name, description, quantity, lastOrdered, location, isInStock }
+}) => {
+  return (
+    <Link className="inventory__list-link" to="id">
+            <ul className="inventory__list-entry">
+              <li className="inventory__list-entry-heading inventory__list-entry-heading--with-kebab">
+                item
+                <img
+                  src={kebabMenu}
+                  className="inventory__list-entry-heading-kebab"
+                  alt="remove item option"
+                />
+              </li>
+              <li className="inventory__list-entry-item-product">
+                Product Name here
+              </li>
+              <li className="inventory__list-entry-item-description">
+                Here is a very brief...
+              </li>
+              <li className="inventory__list-entry-heading">last ordered</li>
+              <li className="inventory__list-entry-item">05/24/2018</li>
+              <li className="inventory__list-entry-heading">location</li>
+              <li className="inventory__list-entry-item">Toronto, CAN</li>
+              <li className="inventory__list-entry-heading">quantity</li>
+              <li className="inventory__list-entry-item">12,000</li>
+              <li className="inventory__list-entry-heading">status</li>
+              <li className="inventory__list-entry-item">In Stock</li>
+              <li className="inventory__list-entry-item-kebab">
+                <img
+                  src={kebabMenu}
+                  className="inventory__list-entry-item-kebab-img"
+                  alt="remove item option"
+                />
+              </li>
+            </ul>
+          </Link>
+  );
+};
 
 export default InventoryItem;
