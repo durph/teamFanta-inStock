af0c9821211b3e474434037b1618f3fe598943db
Vasim <kapadiavasim@gmail.com>
2019-05-27 14:08:57 -0400

    made edits to margin and flex-start

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,13 @@ import {
 import Warehouse from "./components/Warehouse/Warehouse.js";
 import Inventory from "./components/Inventory/Inventory.js";
 import Errors from "./components/Errors/Errors.js";
+import Header from "./components/Header/Header.js";
 
 class App extends Component {
   render() {
     return (
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/warehouse/:warehouseId" component={Warehouse} />
           <Route exact path="/inventory/:inventoryId" component={Inventory} />
