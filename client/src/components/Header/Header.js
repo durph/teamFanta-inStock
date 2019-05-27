79e55a71f044e3009c9694f42d860f9606556158
Vasim <kapadiavasim@gmail.com>
2019-05-27 12:12:15 -0400

    completed header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import instock from "../../assets/Logo/Logo-instock.svg";
-import './Header.scss';
+import "./Header.scss";
+import { Link, NavLink } from "react-router-dom";
 
 class Header extends Component {
   render() {
@@ -8,12 +9,31 @@ class Header extends Component {
       <>
         <header className="header__container">
           <div className="header__logo-container">
-            <img className="header__logo" src={instock} />
-        </div>
-        <ul className="header__link-container">
-            <li className="header__link">Inventory</li>
-            <li className="header__link">Locations</li>
-        </ul>
+            <Link to="/Warehouse">
+              {" "}
+              <img className="header__logo" src={instock} />{" "}
+            </Link>
+          </div>
+          <ul className="header__link-container">
+            <li className="header__link">
+              <NavLink
+                activeClassName="header__container-navlink--active"
+                className="header__container-navlink"
+                to="/Inventory"
+              >
+                Inventory
+              </NavLink>
+            </li>
+            <li className="header__link">
+              <NavLink
+                activeClassName="header__container-navlink--active"
+                className="header__container-navlink"
+                to="/Warehouse"
+              >
+                Locations
+              </NavLink>
+            </li>
+          </ul>
         </header>
       </>
     );
