5a7f1ddaca116a73694f10b7727971a22c2b4bfa
Andrew <suyangsong@gmail.com>
2019-05-27 14:18:02 -0400

    finished feature

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -5,7 +5,29 @@ const inventory = require("../data/inventory.json");
 const getAllInventory = (req, res) => {
   res.json(inventory);
 };
+const postInventory = (req, res) => {
+  Object.values(req.body).forEach(element => {
+    if (!element) {
+      return res.status(400).end();
+    }
+  });
+  const { name, id, description, quantity, lastOrdered, location, isInstock, categories, warehouseID } = req.body;
+  const newInvItem = {
+    id,
+    name,
+    description,
+    quantity,
+    lastOrdered,
+    location,
+    isInstock,
+    categories,
+    warehouseID 
+  };
+  inventory.push(newInvItem);
+  res.json(inventory);
+};
 
 router.get("/", getAllInventory);
+router.post("/", postInventory);
 
 module.exports = router;
