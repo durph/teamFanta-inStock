4ecfade3690c971fcbd74190aa35f8a18c52f8d6
Andre Lowy <andre.lowy@gmail.com>
2019-05-27 19:57:46 -0400

    done WarehouseDetails component for mobile

--- client/src/components/WarehouseDetails/WarehouseDetails.js
+++ client/src/components/WarehouseDetails/WarehouseDetails.js
@@ -8,8 +8,8 @@ import InventoryList from "../InventoryList/InventoryList";
 class WarehouseDetails extends Component {
   state = {
     warehouse: {
-      address:{},
-      contact:{}
+      address: {},
+      contact: {}
     },
     inventory: []
   };
@@ -34,33 +34,55 @@ class WarehouseDetails extends Component {
   render() {
     const { name, address, contact } = this.state.warehouse;
     return (
+      <>
         <main className="warehouse-details">
           <div className="warehouse-details__header">
             <Link to="/warehouse" className="warehouse-details__header-back">
-              <img src={back_arrow} alt="back arrow" className="warehouse-details__header-back-img" />
+              <img
+                src={back_arrow}
+                alt="back arrow"
+                className="warehouse-details__header-back-img"
+              />
             </Link>
             <h1 className="warehouse-details__header-heading">{name}</h1>
           </div>
           <section className="warehouse-details__info">
             <ul className="warehouse-details__info-list">
               <li className="warehouse-details__info-list-label">Address</li>
-              <li className="warehouse-details__info-list-item">{address.street}</li>
-              <li className="warehouse-details__info-list-item">{address.suiteNum}</li>
-              <li className="warehouse-details__info-list-item">{address.city}, {address.province}</li>
-              <li className="warehouse-details__info-list-item">{address.postal}</li>
+              <li className="warehouse-details__info-list-item">
+                {address.street}
+              </li>
+              <li className="warehouse-details__info-list-item">
+                {address.suiteNum}
+              </li>
+              <li className="warehouse-details__info-list-item">
+                {address.city}, {address.province}
+              </li>
+              <li className="warehouse-details__info-list-item">
+                {address.postal}
+              </li>
             </ul>
             <ul className="warehouse-details__info-list">
               <li className="warehouse-details__info-list-label">Contact</li>
-              <li className="warehouse-details__info-list-item">{contact.name}</li>
-              <li className="warehouse-details__info-list-item">{contact.title}</li>
-              <li className="warehouse-details__info-list-item">{contact.phone}</li>
-              <li className="warehouse-details__info-list-item">{contact.email}</li>
+              <li className="warehouse-details__info-list-item">
+                {contact.name}
+              </li>
+              <li className="warehouse-details__info-list-item">
+                {contact.title}
+              </li>
+              <li className="warehouse-details__info-list-item">
+                {contact.phone}
+              </li>
+              <li className="warehouse-details__info-list-item">
+                {contact.email}
+              </li>
             </ul>
           </section>
+        </main>
         <section className="warehouse-details__inventory-list-container">
           <InventoryList inventory={this.state.inventory} />
         </section>
-      </main>
+      </>
     );
   }
 }
